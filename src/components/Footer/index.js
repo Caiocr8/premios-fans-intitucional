6471244e57b4f38197359d8b582a0602ca092f94
Caiocr8 <ccaiocr@gmail.com>
2023-03-21 23:22:49 -0300

    RESPONSIVE FOOTER

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import {AiFillInstagram, AiFillTwitterCircle, AiFillFacebook, AiOutlineWhatsApp} from 'react-icons/ai'
+import './styles.css'
 
 const Footer = () => {
   return (
-    <div className='bg-[#00567b] h-32 w-full flex justify-around items-center'>
+    <div className='bg-[#00567b] h-32 w-full flex justify-around items-center mobile-footer'>
         <div className='flex flex-col'>
             <h1 className=' font-bold'>Quem somos</h1>
             <a href='/' className='cursor-pointer'>Nosso propósito</a>
@@ -14,7 +15,7 @@ const Footer = () => {
             <a href='/' className='cursor-pointer'>Declarações da plataforma</a>
             <a href='/' className='cursor-pointer'>Ajuda e pagamento</a>
         </div>
-        <div className=' flex'>
+        <div>
             <button className=' hover:opacity-[0.1]'><AiFillInstagram className='ml-5'  size={30}/></button>
             <button className=' hover:opacity-[0.1]'><AiFillFacebook className='ml-5' size={30}/></button>
             <button className=' hover:opacity-[0.1]'><AiFillTwitterCircle className='ml-5' size={30}/></button>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -65,8 +65,8 @@ const Home = () => {
           </div>
           <div className='flex justify-center items-center flex-col w-full h-[400px] bg-yellow-400 mt-10 '>
             <h1 className=' text-2xl'>FAÇA PARTE!</h1>
-            <h1 className=' text-7xl'>SEJA UM</h1>
-            <h1 className=' text-7xl'>INFLUENCER</h1>
+            <h1 className=' text-5xl'>SEJA UM</h1>
+            <h1 className=' text-5xl'>INFLUENCER</h1>
             <buttton className=" mt-5 cursor-pointer flex h-10 justify-center items-center w-32 bg-[#00567b]">PARTICIPAR</buttton>
           </div>
         </main>
